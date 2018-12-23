0f17d8b7854232fc7fa37ba2ffd2f23ab5a97b0c
Dhiraj Chhabra <dhirajchhabra630@gmail.com>
2018-12-23 21:02:43 +0530

    added code for appending at Todo list by taking the input

--- assets/To-Do_List.js
+++ assets/To-Do_List.js
@@ -1,1 +1,7 @@
-alert("Connected!!");
+$("input").on("keypress", function(event) {
+	if(event.which === 13) {
+		var inputTodo = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span>X</span> " + inputTodo + "</li>");
+	}
+});
