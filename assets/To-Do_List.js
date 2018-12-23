ca7b4c88e7e0356350f08dfa17aa0e283229478a
Dhiraj Chhabra <dhirajchhabra630@gmail.com>
2018-12-24 00:34:53 +0530

    added code for styling of delete button and corrected the style of input box plus odd color seperation

--- assets/To-Do_List.js
+++ assets/To-Do_List.js
@@ -2,7 +2,7 @@ $("input").on("keypress", function(event) {
 	if(event.which === 13) {
 		var inputTodo = $(this).val();
 		$(this).val("");
-		$("ul").append("<li><span>X</span> " + inputTodo + "</li>");
+		$("ul").append("<li><span><i class='fas fa-trash'></i></span> " + inputTodo + "</li>");
 	}
 });
 
